ca98ff2a60fd4be172ad0fe32cfac1c1340815f5
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-12-21 18:04:23 +0100

    Update AFK.py

--- AFK.py
+++ AFK.py
@@ -4,25 +4,37 @@ created by NeKro
 import time
 from pynput.keyboard import Key, Listener, Controller
 
-
-delay = 3
-presskey = 3
 keyboard = Controller()
+delay = 3
+
+
+def hold_Z (presskey):
+    start = time.time()
+    while time.time() - start < presskey:
+        keyboard.press("z")
+
+def hold_S (presskey):
+    start = time.time()
+    while time.time() - start < presskey:
+        keyboard.press("s")
+
+def hold_Q (presskey):
+    start = time.time()
+    while time.time() - start < presskey:
+        keyboard.press("q")
+
+def hold_D (presskey):
+    start = time.time()
+    while time.time() - start < presskey:
+        keyboard.press("d")
+
 
 while True:
     time.sleep(delay)
-    keyboard.press("z")
-    time.sleep(presskey)
-    keyboard.release("z") 
+    hold_Z(0.5)
     time.sleep(delay)
-    keyboard.press("s")
-    time.sleep(presskey)
-    keyboard.release("s") 
+    hold_S(0.5)
     time.sleep(delay)
-    keyboard.press("q")
-    time.sleep(presskey)
-    keyboard.release("q") 
+    hold_Q(0.5)
     time.sleep(delay)
-    keyboard.press("d")
-    time.sleep(presskey)
-    keyboard.release("d") 
+    hold_D(0.5)
