14cf4a93f2e6ee019123a472ee9d880dbec08228
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-08-12 12:00:55 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+go mod init AntiAFK
+go get fyne.io/fyne/v2
+go get fyne.io/fyne/v2/app
+go get fyne.io/fyne/v2/container
+go get fyne.io/fyne/v2/widget
+go get github.com/micmonay/keybd_event
+rm install.sh
